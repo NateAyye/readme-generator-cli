3c035fea65419ef07ed6c6e7b13ecaf1595afa67
Nathan Cuevas <nathanacuevas97@gmail.com>
2023-05-18 17:03:03 -0700

    Finished most of the generateMarkdown function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,76 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license, userName, projectName) {
+  if (!license) return '';
+  return `![GitHub](https://img.shields.io/github/license/${userName}/${projectName})`;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license, projectName, userName) {
+  if (!license) return '';
+  return `[${license} License](https://github.com/${userName}/${projectName}/blob/main/LICENSE)`;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license, projectName, userName) {
+  if (!license) return '';
+  return `## License
+Refer to the ${renderLicenseLink(
+    license,
+    projectName,
+    userName,
+  )} file within the root of the repository;
+    `;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  const license = data.license === 'none' ? false : data.license;
   return `# ${data.title}
 
+${renderLicenseBadge(license, data.userName, data.title)}
+
+## Description
+
+${data.description}
+
+## Table of Contents
+
+ - [Installation](#installation)
+ - [Usage](#usage)
+ - [Contribution](#contribution)
+ - [Tests](#tests)
+${license ? ' - [License](#license)' : ''}
+ - [Questions](#questions)
+
+## Installation
+
+${data.install}
+
+## Usage
+
+${data.usage}
+
+## Contribution
+
+${data.contribution}
+
+## Tests
+
+${data.tests}
+
+${renderLicenseSection(license)}
+
+## Questions 
+
+Want to know me a little bit bette more? [${
+    data.userName
+  }'s Profile](https://github.com/${data.userName}) 
+
+Want to get in contact with me?
+  - Send an email to ${data.email}  
 `;
 }
 
